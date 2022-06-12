beccfa5bd85395e343cd1332481cd3a84f26ac5f
merlin votat <votatm@gmail.com>
2022-06-12 17:54:21 +0200

    finish

--- main.py
+++ main.py
@@ -4,12 +4,44 @@ def rank(url_set, url_link):
     for url in url_set:
         link_to_me = list(filter(lambda x: x[1] == url, url_link))
         for link in link_to_me:
-            url_set[url] += url_set[link[0]]*0.85
-            url_set[link[0]] *= 0.15
+            url_set[url] += (url_set[link[0]]*0.85)/len(list(filter(lambda x: x[0] == link[0], url_link)))
 
 def iterate_rank(n, set, link):
     for _ in range(0,n):
         rank(set, link)
+        reduce_rank(set, link)
+        if len(link)!=0:
+            distribut_rank(set, link)
+
+def reduce_rank(set, links):
+    filtered = []
+    for link in links:
+        if next((x for x in filtered if x == link[0]), None) is None:
+            filtered.append(link[0])
+
+    for page in filtered:
+        set[page]=set[page]*0.15
+
+def distribut_rank(set, links):
+    def toto(x):
+        val = set[x[0]]
+        set[x[0]]=0
+        return val
+    rest = sum(list(map(toto, links)))
+    for page in set:
+        set[page]+=rest/len(set)
+
+def create_set_from_link(links):
+    filtered = {}
+    for link in links:
+        if next((x for x in filtered if x == link[0]), None) is None:
+            filtered[link[0]] = 0
+        if next((x for x in filtered if x == link[1]), None) is None:
+            filtered[link[0]] = 0
+
+    for page in filtered:
+        filtered[page]=1/len(filtered)
+    return filtered
 
 def read_csv_return_tuple_array(path):
     result = []
--- test.py
+++ test.py
@@ -18,13 +18,35 @@ class TestUtils(unittest.TestCase):
     def test_rank_two_entry_with_ref(self):
         data = {"toto.com":0.5,"titi.com":0.5}
         main.iterate_rank(1, data, [["toto.com", "titi.com"]])
-        self.assertEqual(0.075, data['toto.com'])
-        self.assertEqual(0.925, data['titi.com'])
+        self.assertEqual(0.0375, data['toto.com'])
+        self.assertEqual(0.9625, data['titi.com'])
+
+    def test_rank_three_entry_with_ref(self):
+        data = {"toto.com":1/3,"titi.com":1/3,"tata.com":1/3}
+        main.iterate_rank(1, data, [["toto.com", "titi.com"],["toto.com", "tata.com"]])
+        self.assertEqual(0.016666666666666666, data['toto.com'])
+        self.assertEqual(0.49166666666666664, data['tata.com'])
+        self.assertEqual(0.49166666666666664, data['titi.com'])
+        
+    def test_rank_five_entry_with_ref(self):
+        data = {"toto.com":0.2,"titi.com":0.2,"tutu.com":0.2,"tata.com":0.2,"tete.com":0.2}
+        main.iterate_rank(1, data, [["titi.com", "toto.com"],["tutu.com", "toto.com"],["tata.com", "toto.com"]])
+        self.assertEqual(0.7280000000000001, data['toto.com'])
+        self.assertEqual(0.018, data['titi.com'])
+        self.assertEqual(0.018, data['tutu.com'])
+        self.assertEqual(0.018, data['tata.com'])
+        self.assertEqual(0.218, data['tete.com'])
 
     def test_csv_reading(self):
         result = main.read_csv_return_tuple_array("./testdataset.csv")
         self.assertEqual("http://toto.com", result[0][0])
         self.assertEqual("http://titi.com", result[0][1])
+
+    def test_full(self):
+        result = main.read_csv_return_tuple_array("./dataset.csv")
+        data = main.create_set_from_link(result)
+        main.iterate_rank(1, data, result)
+        self.assertEqual(0.00024011287080542975,data["CGO"])
 
 if __name__ == '__main__':
     unittest.main()
